3aa87a1d6a625ff77b35627383b3745544894e4d
pralhad khadka <puwarpralhad@gmail.com>
2021-04-11 20:29:40 +0545

    Added function

--- swap.cpp
+++ swap.cpp
@@ -1,13 +1,19 @@
 // A program to swap two numbers
 #include <stdio.h>
 
+int swap(int x, int y);
+
 int main(){
-int a, b,temp;
+int a, b,s;
 a=5;
 b=6;
 printf("The value of a and b before swap is %d %d\n",a,b);
-temp = a;
-a=b;
-b= temp;
-printf("The value of and and b after swap is %d %d\n",a,b);
+swap(a ,b);
+}
+int swap(int a, int b ){
+	int temp;
+	temp = a;
+	a=b;
+	b=temp;
+	printf("value of a and b after swap %d %d",a,b);
 }
